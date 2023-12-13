d03e7d4cb17d1bc5fd6ba89684e71dab86405c9a
JonathanXu4 <59599239+JonathanXu4@users.noreply.github.com>
2023-12-12 18:04:49 -0800

    added relic rates

--- src/pages/RelicRates.js
+++ src/pages/RelicRates.js
@@ -2,8 +2,28 @@ import { Link } from "react-router-dom";
 
 const RelicRates = () => {
   return (
-    <div>
-      <h2>relic droprates</h2>
+    <div class="text">
+      <h1>relic stats</h1>
+      <h2>droprates</h2>
+      <p>
+        ar 6: 2.43 relics/run
+        <br /> ar 5: 2 relics/run
+        <br /> these numbers are halved due to 2 sets
+      </p>
+      <h2>mainstats</h2>
+      <p>
+        Chests: 20% HP/ATK/DEF, 10% CR/CD/Healing/EHR
+        <br /> Boots: 30% HP/ATK/DEF, 10% SPD
+        <br /> Sphere: 12% HP/ATK/DEF, 64% Element (/7 = 9.14 per)
+        <br /> Rope: 26% HP/ATK/DEF, 16% Break , 6% ER
+      </p>
+      <h2>substats</h2>
+      <p>
+        10% HP/ATK/DEF
+        <br /> 8% EHR/RES/Break
+        <br /> 6% CR/CD
+        <br /> 4% SPD
+      </p>
     </div>
   );
 };
