3ae14fb502ec1a780fd89d9c8c793bb394cc66ba
JonathanXu4 <59599239+JonathanXu4@users.noreply.github.com>
2023-12-13 13:36:09 -0800

    finished A and B dropdowns

--- src/pages/RelicRates.js
+++ src/pages/RelicRates.js
@@ -1,11 +1,11 @@
 //import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const RelicRates = () => {
-  const [selectedInputA, setSelectedInputA] = useState("");
-  const [selectedInputB, setSelectedInputB] = useState("");
+  const [selectedInputA, setSelectedInputA] = useState("Head");
+  const [selectedInputB, setSelectedInputB] = useState("Hp");
 
-  const piece = ["Hands", "Body", "Feet", "Sphere", "Rope"];
+  const piece = ["Head", "Hands", "Body", "Feet", "Sphere", "Rope"];
   const optionsInputB = {
     Head: ["Hp"],
     Hands: ["Atk"],
@@ -17,38 +17,18 @@ const RelicRates = () => {
 
   const handleInputAChange = (value) => {
     setSelectedInputA(value);
-    setSelectedInputB("Hp");
   };
 
   const handleInputBChange = (value) => {
     setSelectedInputB(value);
   };
 
+  useEffect(() => {
+    setSelectedInputB(optionsInputB[selectedInputA][0]);
+  }, [selectedInputA, optionsInputB]);
+
   return (
     <div>
-      <div class="text">
-        <h1>relic stats</h1>
-        <h2>droprates</h2>
-        <p>
-          ar 6: 2.43 relics/run
-          <br /> ar 5: 2 relics/run
-          <br /> these numbers are halved due to 2 sets
-        </p>
-        <h2>mainstats</h2>
-        <p>
-          Body: 20% HP/ATK/DEF, 10% CR/CD/Healing/EHR
-          <br /> Boots: 30% HP/ATK/DEF, 10% SPD
-          <br /> Sphere: 12% HP/ATK/DEF, 64% Element(/7 = 9.14% each)
-          <br /> Rope: 26% HP/ATK/DEF, 16% Break, 6% ER
-        </p>
-        <h2>substats</h2>
-        <p>
-          10% HP/ATK/DEF/HP%/ATK%/DEF%
-          <br /> 8% EHR/RES/Break
-          <br /> 6% CR/CD
-          <br /> 4% SPD
-        </p>
-      </div>
       <div class="text">
         <label>
           piece:
@@ -56,7 +36,6 @@ const RelicRates = () => {
             value={selectedInputA}
             onChange={(e) => handleInputAChange(e.target.value)}
           >
-            <option value="Head">Head</option>
             {piece.map((option) => (
               <option key={option} value={option}>
                 {option}
@@ -86,6 +65,29 @@ const RelicRates = () => {
             <p>Selected Input B: {selectedInputB}</p>
           </div>
         )}
+      </div>
+      <div class="text">
+        <h4>relic stats</h4>
+        <h5>droprates</h5>
+        <p>
+          ar 6: 2.43 relics/run
+          <br /> ar 5: 2 relics/run
+          <br /> these numbers are halved due to 2 sets
+        </p>
+        <h5>mainstats</h5>
+        <p>
+          Body: 20% HP/ATK/DEF, 10% CR/CD/Healing/EHR
+          <br /> Boots: 30% HP/ATK/DEF, 10% SPD
+          <br /> Sphere: 12% HP/ATK/DEF, 64% Element(/7 = 9.14% each)
+          <br /> Rope: 26% HP/ATK/DEF, 16% Break, 6% ER
+        </p>
+        <h5>substats</h5>
+        <p>
+          10% HP/ATK/DEF/HP%/ATK%/DEF%
+          <br /> 8% EHR/RES/Break
+          <br /> 6% CR/CD
+          <br /> 4% SPD
+        </p>
       </div>
     </div>
   );
