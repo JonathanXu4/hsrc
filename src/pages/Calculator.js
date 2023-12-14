9d95b9efa07231d4adf64f7f224c373959178f35
JonathanXu4 <59599239+JonathanXu4@users.noreply.github.com>
2023-12-14 12:54:28 -0800

    fixed fs error

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,11 +1,4 @@
-import React from "react";
-
-const { StarRail } = require("starrail.js");
-//import StarRail from "starrail.js";
-
 const Calculator = () => {
-  //const client = new StarRail({ defaultLanguage: "en" });
-  //client.getAllLightcones();
   return (
     <div>
       <p></p>
